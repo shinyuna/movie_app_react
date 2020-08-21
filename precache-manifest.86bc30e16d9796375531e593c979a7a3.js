4fd5d20d4deeb0556b53638a9e733e95fa0c38f9
alvinshin <alvin@spinprotocol.io>
2020-08-21 13:31:57 +0900

    Updates

---
+++ precache-manifest.86bc30e16d9796375531e593c979a7a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57fda1f20fa9a3c622a8225b6377ddd3",
+    "url": "/movie_app_react/index.html"
+  },
+  {
+    "revision": "83a24e60297556e62ee7",
+    "url": "/movie_app_react/static/css/main.764a4b03.chunk.css"
+  },
+  {
+    "revision": "b6659f042817e7a27dbd",
+    "url": "/movie_app_react/static/js/2.ffac78c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_react/static/js/2.ffac78c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a24e60297556e62ee7",
+    "url": "/movie_app_react/static/js/main.28f60005.chunk.js"
+  },
+  {
+    "revision": "e772194c85f13f1bd6a9",
+    "url": "/movie_app_react/static/js/runtime-main.b1700b33.js"
+  }
+]);
